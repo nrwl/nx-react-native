f9fa96756261230a4906f806ae38ce9b51bccf3b
Jack Hsu <jack.hsu@gmail.com>
2020-10-27 15:50:12 -0400

    fix(android): update schematics for android and make gradlew executable

--- packages/react-native/src/builders/bundle/bundle.impl.ts
+++ packages/react-native/src/builders/bundle/bundle.impl.ts
@@ -1,18 +1,18 @@
 import { BuilderContext, createBuilder } from '@angular-devkit/architect';
+import { createDirectory, toFileName } from '@nrwl/workspace';
 import { JsonObject } from '@angular-devkit/core';
 import { from, Observable } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
-import { join } from 'path';
+import { dirname, join } from 'path';
 import { getProjectRoot } from '../../utils/get-project-root';
 import { ensureNodeModulesSymlink } from '../../utils/ensure-node-modules-symlink';
 import { fork } from 'child_process';
-import { toFileName } from '@nrwl/workspace';
 
 export interface ReactNativeBuildOptions extends JsonObject {
   dev: boolean;
   platform: string;
   entryFile: string;
-  outputPath: string;
+  bundleOutput: string;
   maxWorkers: number;
   sourceMap: boolean;
 }
@@ -28,7 +28,10 @@ function run(
   context: BuilderContext
 ): Observable<ReactNativeBuildOutput> {
   return from(getProjectRoot(context)).pipe(
-    tap((root) => ensureNodeModulesSymlink(context.workspaceRoot, root)),
+    tap((root) => {
+      createDirectory(dirname(join(root, options.bundleOutput)));
+      ensureNodeModulesSymlink(context.workspaceRoot, root);
+    }),
     switchMap((root) => runCliBuild(context.workspaceRoot, root, options)),
     map(() => {
       return {
@@ -41,7 +44,6 @@ function run(
 function runCliBuild(workspaceRoot, projectRoot, options) {
   return new Promise((resolve, reject) => {
     const cliOptions = createBundleOptions(options);
-    console.log(cliOptions);
     const cp = fork(
       join(workspaceRoot, './node_modules/react-native/cli.js'),
       ['bundle', ...cliOptions],
@@ -65,11 +67,7 @@ function createBundleOptions(options) {
     const v = options[_k];
     const k = toFileName(_k);
     if (v === undefined) return acc;
-    if (k === 'output-path') {
-      acc.push(`--bundle-output`, v);
-    } else {
-      acc.push(`--${k}`, v);
-    }
+    acc.push(`--${k}`, v);
     return acc;
   }, []);
 }
