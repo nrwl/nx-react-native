eab0999cd7af1e8da28dd79a2ec34144c3bd06e9
Jack Hsu <jack.hsu@gmail.com>
2020-10-28 14:17:23 -0400

    feat: add support for workspace libs

--- packages/react-native/plugins/with-nx-metro.ts
+++ packages/react-native/plugins/with-nx-metro.ts
@@ -1,19 +1,21 @@
-import { appRootPath } from '@nrwl/workspace/src/utils/app-root';
 import { join } from 'path';
-import { resolveRequest } from './metro-resolver';
+import { appRootPath } from '@nrwl/workspace/src/utils/app-root';
 import { workspaceLayout } from '@nrwl/workspace/src/core/file-utils';
+import { resolveRequest } from './metro-resolver';
 
 export function withNxMetro(config: any) {
   const watchFolders = config.watchFolders || [];
   const resolver = config.resolver || {};
+
   config.watchFolders = watchFolders.concat([
     join(appRootPath, 'node_modules'),
     join(appRootPath, workspaceLayout().libsDir),
   ]);
-  // TODO: fix resolver and uncomment
-  // config.resolver = {
-  //   ...resolver,
-  //   resolveRequest,
-  // };
+
+  config.resolver = {
+    ...resolver,
+    resolveRequest,
+  };
+
   return config;
 }
