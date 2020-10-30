e6876581f3fbd28736c402ebfc5e701bfd756f4e
Jack Hsu <jack.hsu@gmail.com>
2020-10-29 21:06:01 -0400

    fix: update jest setup

--- packages/react-native/src/utils/versions.ts
+++ packages/react-native/src/utils/versions.ts
@@ -6,3 +6,7 @@ export const reactNativeVersion = '0.63.2';
 export const typesReactNativeVersion = '0.63.18';
 export const reactNativeCommunityCliPlatformIos = '4.13.0';
 export const metroReactNativeBabelPresetVersion = '0.63.0';
+export const testingLibraryReactNativeVersion = '7.1.0';
+export const testingLibraryJestNativeVersion = '3.4.3';
+export const jestReactNativeVersion = '18.0.0';
+export const reactTestRenderer = '17.0.1';
