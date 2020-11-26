841b6ffa01c64101b3d567a3a41e625e1bb7602c
Jack Hsu <jack.hsu@gmail.com>
2020-11-26 11:40:38 -0500

    feat: print more useful info when packager has started

--- packages/react-native/src/builders/start/start.impl.ts
+++ packages/react-native/src/builders/start/start.impl.ts
@@ -1,3 +1,4 @@
+import * as chalk from 'chalk';
 import { BuilderContext, createBuilder } from '@angular-devkit/architect';
 import { from, Observable, of } from 'rxjs';
 import { catchError, map, switchMap, tap } from 'rxjs/operators';
@@ -24,6 +25,15 @@ export function run(
       baseUrl: `http://localhost:${options.port}`,
       success: true,
     })),
+    tap((output) => {
+      const appName = context.target.project;
+      context.logger.info(chalk.cyan(`Packager is ready at ${output.baseUrl}`));
+      context.logger.info(
+        `Use ${chalk.bold(`nx run-android ${appName}`)} or ${chalk.bold(
+          `nx run-ios ${appName}`
+        )} to run the native app.`
+      );
+    }),
     catchError(() => of({ success: false }))
   );
 }
