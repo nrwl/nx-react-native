d9484249a4fe36114d2175254e0ee2430647c021
Jack Hsu <jack.hsu@gmail.com>
2020-12-03 09:10:15 -0500

    chore: update react-native version

--- packages/react-native/src/utils/versions.ts
+++ packages/react-native/src/utils/versions.ts
@@ -2,7 +2,7 @@ export const nxVersion = '10.4.4';
 
 export const reactVersion = '16.13.1';
 export const typesReactVersion = '16.9.38';
-export const reactNativeVersion = '0.63.2';
+export const reactNativeVersion = '0.63.4';
 export const typesReactNativeVersion = '0.63.18';
 export const reactNativeCommunityCliIos = '4.13.0';
 export const metroReactNativeBabelPresetVersion = '0.63.0';
