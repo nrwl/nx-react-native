e8c50cbdd952b53a80890b9d1cdb0b5c8d67ed53
Jack Hsu <jack.hsu@gmail.com>
2020-12-15 12:29:02 -0500

    fix: compatibility with Nx 11

--- packages/react-native/src/utils/get-project-root.ts
+++ packages/react-native/src/utils/get-project-root.ts
@@ -1,22 +1,12 @@
 import { BuilderContext } from '@angular-devkit/architect';
-import { workspaces } from '@angular-devkit/core';
-import { NodeJsSyncHost } from '@angular-devkit/core/node';
-import { join } from 'path';
 
-export async function getProjectRoot(context: BuilderContext) {
-  const workspaceHost = workspaces.createWorkspaceHost(new NodeJsSyncHost());
-  const { workspace } = await workspaces.readWorkspace(
-    context.workspaceRoot,
-    workspaceHost
-  );
-  if (workspace.projects.get(context.target.project).root) {
-    return join(
-      context.workspaceRoot,
-      workspace.projects.get(context.target.project).root
-    );
+export async function getProjectRoot(context: BuilderContext): Promise<string> {
+  const projectMeta = await context.getProjectMetadata(context.target.project);
+  if (projectMeta.root) {
+    return projectMeta.root as string;
   } else {
     context.reportStatus('Error');
-    const message = `${context.target.project} does not have a root. Please define one.`;
+    const message = `${context.target.project} does not have a sourceRoot. Please define one.`;
     context.logger.error(message);
     throw new Error(message);
   }
