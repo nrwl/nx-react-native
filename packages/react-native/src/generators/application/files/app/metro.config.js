f02e11ae6c612ede8fc54fe1158718209b4f0d38
Emily Xiong <xiongemi@gmail.com>
2021-05-18 17:58:44 -0400

    update package versions

--- packages/react-native/src/generators/application/files/app/metro.config.js
+++ packages/react-native/src/generators/application/files/app/metro.config.js
@@ -1,15 +1,19 @@
 const { withNxMetro } = require('@nrwl/react-native');
-module.exports = withNxMetro({
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
+
+module.exports = withNxMetro(
+  {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
   },
-}, {
-  // Change this to true to see debugging info.
-  // Useful if you have issues resolving modules
-  debug: false
-});
+  {
+    // Change this to true to see debugging info.
+    // Useful if you have issues resolving modules
+    debug: false,
+  }
+);
