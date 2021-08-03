7018a2bd0c82f19dac97d97e4f7e29d4f2ed6610
Emily Xiong <xiongemi@gmail.com>
2021-08-03 00:22:45 -0400

    add ios e2e for react native

--- e2e/detox-e2e/tests/detox.test.ts
+++ e2e/detox-e2e/tests/detox.test.ts
@@ -3,6 +3,7 @@ import {
   runNxCommandAsync,
   uniq,
 } from '@nrwl/nx-plugin/testing';
+import { platform } from 'os';
 
 describe('Detox', () => {
   test('should create files and run lint command', async () => {
@@ -18,6 +19,24 @@ describe('Detox', () => {
     checkFilesExist(`apps/${appName}-e2e/src/app.spec.ts`);
 
     await expect(runNxCommandAsync(`test ${appName}`)).resolves.toMatchObject({
+      stdout: expect.any(String),
+    });
+  });
+
+  test('should test ios e2e', async () => {
+    if (platform() !== 'darwin') {
+      return;
+    }
+
+    const appName = uniq('myapp');
+    await runNxCommandAsync(
+      `generate @nrwl/react-native:app ${appName} --e2eTestRunner=detox --linter=eslint`
+    );
+
+    await runNxCommandAsync(`build-ios ${appName}-e2e`);
+    await expect(
+      await runNxCommandAsync(`test-ios ${appName}-e2e --cleanup`)
+    ).resolves.toMatchObject({
       stdout: expect.any(String),
     });
   });
